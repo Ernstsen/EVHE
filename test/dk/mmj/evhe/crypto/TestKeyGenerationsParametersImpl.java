8d537f0c2d10833b8b86411a208d4f5607a3a8a2
morten <201608514@post.au.dk>
2019-02-27 14:27:34 +0100

    Added extra test case for generator g

--- test/dk/mmj/evhe/crypto/TestKeyGenerationsParametersImpl.java
+++ test/dk/mmj/evhe/crypto/TestKeyGenerationsParametersImpl.java
@@ -5,13 +5,31 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TestKeyGenerationsParametersImpl {
+
+    /**
+     * Tests if generator found for P of length 4 bits is correct
+     * With length of 4 bits, it can only be that p = 2*q + 1 = 2*5 + 1 = 11
+     */
     @Test
     public void testGeneratorForPBitLength4() {
-        // With 4 bits it can only be that p = 2*q +1 = 2*5 + 1 = 11
         KeyGenerationParameters params = new KeyGenerationParametersImpl(4, 50);
-        List<Integer> possibleGeneratorsForP = Arrays.asList(4,36,49,72);
-        Assert.assertTrue(possibleGeneratorsForP.contains(params.getGenerator().intValue()));
+        List<Integer> possibleGeneratorsForP = Arrays.asList(2, 6, 7, 8);
+        List<Integer> possibleGeneratorsForQ = possibleGeneratorsForP.stream().map(i -> (int) Math.pow(i.doubleValue(), 2.0)).collect(Collectors.toList());
+        Assert.assertTrue(possibleGeneratorsForQ.contains(params.getGenerator().intValue()));
+    }
+
+    /**
+     * Tests if generator found for P of length 5 bits is correct
+     * With length of 5 bits, it can only be that p = 2*q + 1 = 2*11 + 1 = 23
+     */
+    @Test
+    public void testGeneratorForPBitLength5() {
+        KeyGenerationParameters params = new KeyGenerationParametersImpl(5, 50);
+        List<Integer> possibleGeneratorsForP = Arrays.asList(5, 7, 10, 11, 14, 15, 17, 19, 20, 21);
+        List<Integer> possibleGeneratorsForQ = possibleGeneratorsForP.stream().map(i -> (int) Math.pow(i.doubleValue(), 2.0)).collect(Collectors.toList());
+        Assert.assertTrue(possibleGeneratorsForQ.contains(params.getGenerator().intValue()));
     }
 }
