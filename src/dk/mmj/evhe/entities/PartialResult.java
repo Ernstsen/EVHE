3d700a2e8006b9e94fa9362aed60de4e6172f714
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2019-05-13 11:36:33 +0200

    added sum and #votes for partialResult

--- src/dk/mmj/evhe/entities/PartialResult.java
+++ src/dk/mmj/evhe/entities/PartialResult.java
@@ -9,14 +9,18 @@ public class PartialResult {
     private Integer id;
     private BigInteger result;
     private DLogProofUtils.Proof proof;
+    private CipherText c;
+    private int votes;
 
     public PartialResult() {
     }
 
-    public PartialResult(Integer id, BigInteger result, DLogProofUtils.Proof proof) {
+    public PartialResult(Integer id, BigInteger result, DLogProofUtils.Proof proof, CipherText c, int votes) {
         this.id = id;
         this.result = result;
         this.proof = proof;
+        this.c = c;
+        this.votes = votes;
     }
 
     public Integer getId() {
@@ -41,6 +45,22 @@ public class PartialResult {
 
     public void setProof(DLogProofUtils.Proof proof) {
         this.proof = proof;
+    }
+
+    public CipherText getC() {
+        return c;
+    }
+
+    public void setC(CipherText c) {
+        this.c = c;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
     }
 }
 
