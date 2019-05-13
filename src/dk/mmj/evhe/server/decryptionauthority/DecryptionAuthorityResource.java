b2c69b7374df1922191f429ade5c741c4c0075e2
Mathias Jensen <darkdawnmsj@gmail.com>
2019-05-13 11:35:07 +0200

    Introduced the option to create a corrupt Decryption Authority. This can be done using --corrupt=time.

--- src/dk/mmj/evhe/server/decryptionauthority/DecryptionAuthorityResource.java
+++ src/dk/mmj/evhe/server/decryptionauthority/DecryptionAuthorityResource.java
@@ -1,36 +1,23 @@
 package dk.mmj.evhe.server.decryptionauthority;
 
-import dk.mmj.evhe.server.ServerState;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import static dk.mmj.evhe.server.decryptionauthority.DecryptionAuthority.SERVER;
-
 @Path("/")
 public class DecryptionAuthorityResource {
     private static Logger logger = LogManager.getLogger(DecryptionAuthorityResource.class);
-    private ServerState state = ServerState.getInstance();
 
     @GET
-    @Path("type")//TODO: REMOVE
+    @Path("type")
     @Produces(MediaType.TEXT_HTML)
     public String test() {
         logger.info("Received request for server type");
 
-        return "<b>ServerType:</b> Key Server";
-    }
-
-    @POST
-    @Path("terminate")
-    public void terminate() {
-        DecryptionAuthority server = state.get(SERVER, DecryptionAuthority.class);
-        logger.info("Terminating voting");
-        server.terminateVoting();
+        return "<b>ServerType:</b> Decryption Authority";
     }
 }
